47790113ba0c9ed41a8b242bb8666ebbba85341d
sammyoina <sammyoina@gmail.com>
2020-06-26 19:29:10 +0300

    package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='statDistributions',
+      version='1.0',
+      description='statistical distributions',
+      packages=['distributions'],
+      zip_safe=False)
